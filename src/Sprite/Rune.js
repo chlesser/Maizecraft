e5b0de6a9b901a8a36d61b818efacd46d3a2baf8
Charles Lesser <clesser@ucsc.edu>
2025-06-10 03:02:40 -0700

    potential future for runes

--- src/Sprite/Rune.js
+++ src/Sprite/Rune.js
@@ -6,6 +6,7 @@ class Rune extends Phaser.GameObjects.Sprite {
         this.setDepth(1);
         this.type = type; // Type of rune (e.g., 'cooldown', 'damage', 'range', 'frost')
         this.level = level; // Level of the rune (1, 2, or 3)
+        this.selected = false;
         this.makeMeSpecial(); // Set the texture based on type and level
         scene.add.existing(this);
     }
@@ -13,7 +14,11 @@ class Rune extends Phaser.GameObjects.Sprite {
         const concat = this.type + "Rune" + this.level;
         this.setTexture(concat); // Default texture for rune
     }
+    Select() {
+        this.selected = true;
+        this.setTint(0x00ff00); // Change color to indicate selection
+    }
     update() {
-        // Update logic for the rune can be added here
+        
     }
 }
