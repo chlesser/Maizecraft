80de4ba607589ac90f2aae4754fa5eaa529ac303
Charles Lesser <clesser@ucsc.edu>
2025-06-10 23:22:49 -0700

    initial projectiles

---
+++ src/Sprite/Arrow.js
@@ -0,0 +1,29 @@
+class Arrow extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, texture, frame) {        
+        super(scene, x, y, texture, frame);
+        this.texture = 'arrow';
+        this.visible = false;
+        this.active = false;
+        return this;
+    }
+
+    update() {
+        if (this.active) {
+            this.y -= this.speed;
+            if (this.y < -(this.displayHeight/2)) {
+                this.makeInactive();
+            }
+        }
+    }
+
+    makeActive() {
+        this.visible = true;
+        this.active = true;
+    }
+
+    makeInactive() {
+        this.visible = false;
+        this.active = false;
+    }
+
+}
