e30bd67b46cf6eae01806a5b093c6c7662aa0c16
Charles Lesser <clesser@ucsc.edu>
2025-06-11 22:33:05 -0700

    credits

--- src/Scenes/Lose.js
+++ src/Scenes/Lose.js
@@ -13,11 +13,11 @@ class Lose extends Phaser.Scene {
     // title card background image
     this.add.image(width / 2, height / 2, "theend").setDisplaySize(1280, 800);
 
-    // Game title
+    // Round Count
     let txt = this.add.bitmapText(width / 2, height / 2 - 150, "pixelFont", 'You made it to round ' + data.wave, 50).setOrigin(0.5);
 
 
-    // Play Button
+    // Restart Button
     const playButton = this.add.bitmapText(width / 2, height / 2 - 250, "pixelFont", "Restart", 80)
         .setOrigin(0.5)
         .setInteractive();
@@ -25,6 +25,18 @@ class Lose extends Phaser.Scene {
     playButton.on('pointerdown', () => {
       this.scene.start('titleScene');
     });
+
+    const INITIAL = 100;
+    const creditSpacing = 50;
+    let i = 0;
+    this.add.bitmapText(width / 2, height / 2 + INITIAL + (creditSpacing * i), "pixelFont", 'Programming: Charles Lesser, Athea Reynolds, Tina Huynh', 30).setOrigin(0.5);
+    i++;
+    this.add.bitmapText(width / 2, height / 2 + INITIAL + (creditSpacing * i), "pixelFont", 'Sound Engineering: Athea Reynolds', 30).setOrigin(0.5);
+    i++;
+    this.add.bitmapText(width / 2, height / 2 + INITIAL + (creditSpacing * i), "pixelFont", 'Voices: Charles Lesser, Athea Reynolds, Tina Huynh', 30).setOrigin(0.5);
+    i++;
+    this.add.bitmapText(width / 2, height / 2 + INITIAL + (creditSpacing * i), "pixelFont", 'Sound Effects/Music: Mixkit.com', 30).setOrigin(0.5);
+    i++;
+    this.add.bitmapText(width / 2, height / 2 + INITIAL + (creditSpacing * i), "pixelFont", 'Art: Kenney Assets', 30).setOrigin(0.5);
     }
-    
 }
